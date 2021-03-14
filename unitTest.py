d53e56545377d759b79e674367d950dc8f66751a
pyOwNii <owniigame@gmail.com>
2021-03-14 18:12:09 +0100

    Simple Unit Test

---
+++ unitTest.py
@@ -0,0 +1,19 @@
+
+import unittest
+
+# Testar en simpel addition
+
+def addition(x,y):
+    z = x + y
+    return z
+
+class testAddition(unittest.TestCase):
+    def testAddition(self):
+        # Viktigt att komma ihåg att vid unittest så testar vi mot en expected outcome.
+        # Addition, the inputs against the expected outcome
+        self.assertEqual(addition(50,50), 100)
+
+if __name__ == '__main__':
+    unittest.main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
